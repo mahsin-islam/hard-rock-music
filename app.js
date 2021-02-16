8f59baf1fad5a09e3b74483c876cc71a3fff35ca
MD. MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2021-02-16 19:25:21 +0600

    Completed Musiz lyrics app

---
+++ app.js
@@ -0,0 +1,73 @@
+const searchSongs = () => {
+    const searchText = document.getElementById('search-filed').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+    // load data
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySongs(data.data))
+        .catch(error => displayError('Something Went Wrong!! Please try again later.'));
+        // .catch(error => console.log(error));
+}
+// const searchSongs = async() => {
+//     const searchText = document.getElementById('search-filed').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+//     // load data
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displaySongs(data.data);
+// }
+const displaySongs = songs => {
+    const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        // console.log(song);
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+                <source src="${song.preview}" type="audio/ogg">
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onClick="getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>
+        `;
+        songContainer.appendChild(songDiv);
+    })
+}
+
+// const getLyric = (artist, title) => {
+//     // console.log(artist, title);
+//     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => displayLyrics(data.lyrics))
+// }
+const getLyric = async (artist, title) => {
+    // console.log(artist, title);
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    try{
+        console.log(url);
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+    }
+    catch(error){
+        displayError('Sorry! I failed to load lyrics, Please try again letter!');
+        // console.log(error);
+    };
+    
+}
+const displayLyrics = lyrics => {
+    console.log(lyrics);
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerHTML = lyrics;
+}
+
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error;
+}
